cdcfa356fd640b940dd83a507cc39566d57c9e3f
Administrator <364570271@qq.com>
2018-04-17 21:15:07 +0800

    修改地址

--- mall-admin-web/src/main/java/com/mall/admin/service/fallback/FeignUsersException.java
+++ mall-admin-web/src/main/java/com/mall/admin/service/fallback/FeignUsersException.java
@@ -3,11 +3,24 @@ package com.mall.admin.service.fallback;
 import org.springframework.stereotype.Component;
 
 import com.mall.admin.service.FeignUserService;
+import com.mall.uums.model.Users;
 @Component
 public class FeignUsersException implements FeignUserService{
 
 	public String sayHiFromClientOne(String name) {
 		return "error:"+name;
+	}
+
+	@Override
+	public Users testa() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String testadd() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
